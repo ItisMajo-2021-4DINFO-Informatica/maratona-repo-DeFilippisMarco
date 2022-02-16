6d9c799a18a5a3a18ced6a03c02962a906e65d77
DeFilippisMarco <97604413+DeFilippisMarco@users.noreply.github.com>
2022-02-16 14:15:12 +0100

    app maratona
    prima versione non completa con solo le classi

---
+++ DeFilippisMaratonaApp/DeFilippisMaratonaApp/Maratona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeFilippisMaratonaApp
+{
+    class Maratona
+    {
+        public string NomeAtleta { get; set; }
+        public string SocietàDiAppartenenza { get; set; }
+        public string TempoImpiegato { get; set; }
+        public string Città { get; set; }
+    }
+}
---
+++ DeFilippisMaratonaApp/DeFilippisMaratonaApp/Maratone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeFilippisMaratonaApp
+{
+    class Maratone
+    {
+        public List<Maratona> elencoMaratone;
+        public maratone()
+        {
+            elencoMaratone = new List<Maratona>();
+        }
+    }
+    
+}
